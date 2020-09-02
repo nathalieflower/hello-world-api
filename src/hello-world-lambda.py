a05a1efad84c84fb23ab652306f61b5769592ab6
Nathalie Flower <nathalie@bango.com>
2020-09-02 17:02:41 +0100

    fix(main): fixing date time bug in lambda

--- src/hello-world-lambda.py
+++ src/hello-world-lambda.py
@@ -1,10 +1,9 @@
 import datetime
 import json
 
-current_time = datetime.datetime.now()
-
 
 def main_handler(event, context):
+    current_time = datetime.datetime.now()
     return {
         "statusCode": 200,
         "body": json.dumps('Hello World : ' + str(current_time))
