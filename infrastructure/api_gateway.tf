29e9c57636d4a19ef4a35e84de38d419227e996d
Nathalie Flower <nathalie@bango.com>
2020-09-03 09:40:35 +0100

    refactor(terraform): moving api gateway code into api_gateway.tf

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -2,7 +2,7 @@ resource "aws_api_gateway_rest_api" "hello-world-api-gateway" {
   name        = "Hello-World-Api-Gateway"
 }
 
-resource "aws_api_gateway_deployment" "example" {
+resource "aws_api_gateway_deployment" "api_deploy" {
   depends_on = [
     aws_api_gateway_integration.lambda,
     aws_api_gateway_integration.lambda_root,
@@ -12,6 +12,46 @@ resource "aws_api_gateway_deployment" "example" {
   stage_name  = "test"
 }
 
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.hello-world-api-gateway.id
+  parent_id   = aws_api_gateway_rest_api.hello-world-api-gateway.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.hello-world-api-gateway.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.hello-world-api-gateway.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hello_world_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = aws_api_gateway_rest_api.hello-world-api-gateway.id
+  resource_id   = aws_api_gateway_rest_api.hello-world-api-gateway.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = aws_api_gateway_rest_api.hello-world-api-gateway.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hello_world_lambda.invoke_arn
+}
+
 output "base_url" {
-  value = aws_api_gateway_deployment.example.invoke_url
+  value = aws_api_gateway_deployment.api_deploy.invoke_url
 }
